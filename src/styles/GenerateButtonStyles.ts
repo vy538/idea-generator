566e34a625c8f0cb80a4d72fc1c6d086bf298f94
vy538 <good5538@gmail.com>
2024-07-29 13:23:06 -0400

    generate btn style update

--- src/styles/GenerateButtonStyles.ts
+++ src/styles/GenerateButtonStyles.ts
@@ -2,21 +2,27 @@ import styled from 'styled-components';
 import { theme } from './theme';
 
 export const StyledButton = styled.button<{ lang: 'en' | 'zh' }>`
-  background-color: ${theme.colors.primaryText};
+  background-color: ${theme.colors.background};
   border: none;
-  color: ${theme.colors.secondaryAccent};
-  padding: 15px 30px;
+  color: ${theme.colors.primaryText};
+  padding: 32px 72px;
   text-align: center;
   text-decoration: none;
   display: inline-block;
   font-size: 84px;
   font-weight: 700;
   cursor: pointer;
-  border-radius: 30px;
+  border-radius: 150px;
+  border-style: solid;
+  border-width: 8px;
+  border-color: ${theme.colors.primaryText};
+  box-shadow: 10px 10px ${theme.colors.primaryText};
   transition: background-color 0.3s ease;
   font-family: ${props => theme.fonts[props.lang].family};
 
   &:hover {
     background-color: ${theme.colors.buttonHover};
+    transform: translate(10px,10px);
+    box-shadow: 0px 0px ${theme.colors.primaryText};
   }
 `;
